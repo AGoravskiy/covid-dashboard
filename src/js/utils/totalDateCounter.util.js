4df0bbf97171c1a50c61c38bf652d0349e32078d
Aleksey Goravskiy <32121103+AGoravskiy@users.noreply.github.com>
2020-12-24 13:48:53 +0300

    fix(layout): fix grid-template-rows

--- src/js/utils/totalDateCounter.util.js
+++ src/js/utils/totalDateCounter.util.js
@@ -2,7 +2,6 @@ function totalDateCount() {
   const pandemiaStart = new Date(2020, 0, 22, 0, 0, 0, 0);
   const currentDate = new Date();
   const timeConvert = 86400000;
-  console.log(Math.round((currentDate - pandemiaStart) / timeConvert));
   return Math.round((currentDate - pandemiaStart) / timeConvert);
 }
 
